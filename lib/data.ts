2a2e626002d3ad7afbd3b8033eea17a3b0f0e255
sharvarii1 <sharvaripawar1112@gmail.com>
2025-04-12 23:53:31 +0530

    update

--- lib/data.ts
+++ lib/data.ts
@@ -70,7 +70,7 @@ export const projectsData = [
       "Used YOLOv9 model for detecting road anomalies like potholes and cracks with high precision.",
       "Visualized detected anomalies on interactive maps using Leaflet.",
       "Built a React.js frontend to display real-time results and mapped coordinates.",
-      "Collaborated with mentor A.M. Jagtap and integrated spatial data pipelines."
+     "Collaborated with mentor A.M. Jagtap and followed an effective project development process from design to deployment."
     ],
     repoLink: "https://github.com/sharvarii1/Pdf_Q-A_chatbot",
   },
@@ -88,14 +88,14 @@ export const projectsData = [
     repoLink: "https://github.com/sharvarii1/Potholes-Mapping",
   },
   {
-    title: "Customer Churn Prediction",
+    title: "Employee Churn Prediction",
   description:
-    "Developed a machine learning system to predict customer churn using best practices in feature engineering and classification with Artificial Neural Networks (ANN).",
+    "Developed a machine learning system to predict employee churn using best practices in feature engineering and classification with Artificial Neural Networks (ANN).",
   tags: ["TensorFlow", "ANN", "Machine Learning", "Feature Engineering", "Streamlit"],
   points: [
     "Performed data preprocessing and feature engineering to enhance model accuracy.",
     "Built a classification model using Artificial Neural Networks (ANN) with TensorFlow.",
-    "Trained the model to predict whether a customer will stay or leave based on historical data.",
+    "Trained the model to predict whether a employee will stay or leave based on historical data.",
     "Deployed the model using Streamlit for interactive and user-friendly access."
     ],
     repoLink: "https://github.com/sharvarii1/Company_churn_analysis",
