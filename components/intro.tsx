059c1a6c478ce1b63b45bf6acece94d6a482e0a4
sharvarii1 <sharvaripawar1112@gmail.com>
2025-04-24 11:04:06 +0530

    leetcode add

--- components/intro.tsx
+++ components/intro.tsx
@@ -104,7 +104,7 @@ export default function Intro() {
         
 <a
   className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-  href="https://leetcode.com/yourusername"
+  href="https://leetcode.com/sharvaripawar1112"
   target="_blank"
 >
   <SiLeetcode />
